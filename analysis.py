94d6c7c19d30a1f0ea6a7baee684304fe34fa015
Preston Boysen <psboysen22@icloud.com>
2026-07-16 09:06:46 -0600

    new Demonstration

--- analysis.py
+++ analysis.py
@@ -1,5 +1,11 @@
 import pandas as pd
 df = pd.read_csv("sales.csv")
-print("N Rows:", len(df))
-print("Mean sale", df["amount"].mean())
-print("Largest Sale", df["amount"].max())
+print("Number of transacations:", len(df))
+print("Mean transaction sale", df["price"].mean())
+# Print the Largest Sale: 
+print("Largest transaction", df["price"].max())
+# Print the Smallest sale: 
+print("Smallest transaction", df["price"].min())
+#Print the standard deviation of the transaction prices: 
+print("Standard deviation of transaction prices:", df["Pric"e].sd())
+
